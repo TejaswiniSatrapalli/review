3e597268c4de346b5999012fdb143f0e6c747701
TejaswiniSatrapalli <121703541+TejaswiniSatrapalli@users.noreply.github.com>
2023-02-25 23:23:15 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-
-import { RoomsComponent } from './rooms/rooms.component';
-import { BookingsComponent } from './bookings/bookings.component';
-import { TransactionsComponent } from './transactions/transactions.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { ReviewComponent } from './review/review.component';
-import { UserComponent } from './user/user.component';
-import { HotelComponent } from './hotel/hotel.component';
-import { ViewReviewsComponent } from './view-reviews/view-reviews.component';
-
-const allLinks:Routes=[
-
-  {path:'hotels',component:HotelComponent},
-  {path:'review',component:ReviewComponent},
-  {path:'rooms',component:RoomsComponent},
-  {path:'bookings',component:BookingsComponent},
-  {path:'transactions',component:TransactionsComponent},
-  {path:'about-us',component:AboutUsComponent},
-  {path:'viewReviews',component:ViewReviewsComponent},
+import { BookingComponent } from './booking/booking.component';
+import { ViewBookingsComponent } from './view-bookings/view-bookings.component';
+import { BookingDetailsComponent } from './booking-details/booking-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ViewByDateComponent } from './view-by-date/view-by-date.component';
+import { ViewByIdComponent } from './view-by-id/view-by-id.component';
 
 
+const allLinks:Routes = [
+  {path:'addBooking',component:BookingComponent},
+  {path:'bookings',component:BookingDetailsComponent},
+  {path:'bookingsId',component:ViewBookingsComponent},
+  {path:'viewById',component:ViewByIdComponent},
+  {path:'viewByDate',component:ViewByDateComponent}
+ 
 ]
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent,
-    HotelComponent,
-    RoomsComponent,
-    BookingsComponent,
-    TransactionsComponent,
-    HeaderComponent,
-    ReviewComponent,
-    UserComponent,
-    HotelComponent,
-    ViewReviewsComponent,
-  
+    BookingComponent,
+    ViewBookingsComponent,
+    BookingDetailsComponent,
+    ViewByDateComponent,
+    ViewByIdComponent,
+
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(allLinks),
-    FormsModule,
-    HttpClientModule
+    FormsModule,BrowserModule,RouterModule.forRoot(allLinks),HttpClientModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-bookings/view-bookings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BookingDTO } from '../booking-dto';
+import { BookingOperationService } from '../booking-operation.service';
+
+@Component({
+  selector: 'app-view-bookings',
+  templateUrl: './view-bookings.component.html',
+  styleUrls: ['./view-bookings.component.css']
+})
+export class ViewBookingsComponent {
+
+}
